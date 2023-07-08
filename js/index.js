b2e2f746cc289cde22dd5f044ff75c63f15ed412
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-07-08 12:52:22 +0300

    Initialize starting variables

--- js/index.js
+++ js/index.js
@@ -13,3 +13,22 @@ function getEventsData() {
       selectEvent(data);
     });
 }
+
+// Starting variables
+const signupBtn = document.querySelector(".signup-btn");
+const eventForm = document.querySelector(".event-form");
+const eventsContainer = document.querySelector(".events-container");
+const events = document.querySelector(".events");
+const searchBtn = document.querySelector(".search-btn");
+const selectEvents = document.querySelector("#events");
+const eventType = document.querySelector(".event-type");
+const musicGenre = document.querySelector(".music-genre");
+const locationSearchBar = document.querySelector(".location-search-bar");
+const eventSearchBar = document.querySelector(".search-bar");
+const purchaseTicketbtn = document.querySelector(".purchase-ticket");
+const createEventBtn = document.querySelector(
+  ".search-event .create-event-btn"
+);
+const submitEventFormBtn = document.querySelector(
+  ".event-form .create-event-btn"
+);
